7f98ad43e15bcf50d5b63af40f35ac6ade141ac1
rhea-2000 <jananirhea20@gmail.com>
2022-01-13 14:37:30 +0000

    pipeline added

---
+++ pipeline/jenkins/test/mvn.sh
@@ -0,0 +1,7 @@
+i#!/bin/bash
+
+echo "***************************"
+echo "**Testing the code*********"
+echo "***************************"
+
+docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.8.2-ibmjava-alpine "$@"
